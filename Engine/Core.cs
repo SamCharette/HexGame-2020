e1a81e9a574d9caa277f957f6b0560b6aa96b19b
Sam Charette <spamforsam@worldwidesam.net>
2020-01-19 18:35:48 -0500

    Added some basic functionality to the game engine.

---
+++ Engine/Core.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine
+{
+    public class Engine
+    {
+        // Board size must be equal in both directions
+        public int Size;
+        public List<Hex> Board;
+        private HexOwner lastPlayer;
+
+        // we need a player 1
+
+        // we need a player 2
+
+        // We need a way to get input from a player
+
+        // We need a way to send the new board and game state to a player
+
+
+        // functions
+        public Engine(int size = 11)
+        {
+            Size = size;
+            lastPlayer = HexOwner.Player1;
+            NewBoard();
+        }
+
+        public void TakeTurn(HexOwner player, int x, int y)
+        {
+            if (player == lastPlayer)
+            {
+                throw new Exception("Cannot play twice in a row");
+            }
+
+            if (CheckHex(x, y))
+            {
+                AssignHex(x, y, player);
+                lastPlayer = player;
+            }
+
+        }
+
+        public bool CheckHex(int x, int y)
+        {
+            var hexToCheck = Board?.First(hex => hex.X == x && hex.Y == y);
+
+            if (hexToCheck == null) return false;
+
+            return hexToCheck.Owner == HexOwner.Empty;
+        }
+
+        private void AssignHex(int x, int y, HexOwner owner)
+        {
+            var hexToClaim = Board?.First(hex => hex.X == x && hex.Y == y);
+
+            if (hexToClaim != null)
+            {
+                if (hexToClaim.Owner == HexOwner.Empty)
+                {
+                    hexToClaim.Owner = owner;
+                }
+                else
+                {
+                    throw new Exception("Hex is already taken");
+                }
+            }
+            else
+            {
+                throw new Exception("Could not find hex to claim");
+            }
+        }
+
+        public void NewBoard()
+        {
+            Board = new List<Hex>();
+
+            for (var i = 0; i < Size; i++)
+            {
+                for (var j = 0; j < Size; j++)
+                {
+                    var hex = new Hex(i, j);
+                    Board.Add(hex);
+                }
+            }
+        }
+    }
+}
---
+++ Engine/Hex.cs
@@ -0,0 +1,23 @@
+﻿namespace Engine
+{
+
+    public enum HexOwner
+    {
+        Empty,
+        Player1,
+        Player2
+    }
+
+    public class Hex
+    {
+        public int X;
+        public int Y;
+        public HexOwner Owner;
+
+        public Hex(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}

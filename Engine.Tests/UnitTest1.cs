2f3da5305c7de541f86e26217e60bbb8c2bd0a8f
Sam Charette <spamforsam@worldwidesam.net>
2020-01-21 11:29:33 -0500

    Added test for out of range

--- Engine.Tests/UnitTest1.cs
+++ Engine.Tests/UnitTest1.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Linq;
 using NUnit.Framework;
 
 namespace Engine.Tests
 {
+    [TestFixture]
     public class EngineTests
     {
 
@@ -10,15 +12,24 @@ namespace Engine.Tests
         [SetUp]
         public void Setup()
         {
-            _engine = new global::Engine.Engine(11);
+            _engine = new Engine(11);
         }
 
-
+        
         [Test]
         public void NewEngineBoardShouldBeEmpty()
         {
             int numberOfTakenHexes = _engine.Board.Count(x => x.Owner != HexOwner.Empty);
             Assert.AreEqual(0, numberOfTakenHexes);
+        }
+
+        [Test]
+        public void SelectingOutOfRangeHexShouldFail()
+        {
+            Assert.That(() => _engine.TakeTurn(HexOwner.Player1, 1, -1), Throws.TypeOf<Exception>());
+            Assert.That(() => _engine.TakeTurn(HexOwner.Player1, -1, 1), Throws.TypeOf<Exception>());
+            Assert.That(() => _engine.TakeTurn(HexOwner.Player1, 1, 12), Throws.TypeOf<Exception>());
+            Assert.That(() => _engine.TakeTurn(HexOwner.Player1, 12, 1), Throws.TypeOf<Exception>());
         }
     }
 }

c0eeb78ad3840dd16a680aa325f826fcb9176dba
Sam Charette <spamforsam@worldwidesam.net>
2020-02-20 10:11:32 -0500

    Updated and added items

--- HexLibrary/Hex.cs
+++ HexLibrary/Hex.cs
@@ -15,6 +15,16 @@ namespace HexLibrary
         Left
     }
 
+    public enum AxialDiagonalDirections
+    {
+        Top,
+        TopLeft,
+        BottomLeft,
+        Bottom,
+        BottomRight,
+        TopRight
+    }
+
     public class Hex
     {
         public int q; // column
@@ -35,6 +45,16 @@ namespace HexLibrary
             { AxialDirections.Left, new Tuple<int, int>(-1, 0) }
         };
 
+        // TO DO This isn't set up correctly for the appropriate value changes
+        public Dictionary<AxialDiagonalDirections, Tuple<int, int>> DiagonalDirections = new Dictionary<AxialDiagonalDirections, Tuple<int, int>>()
+        {
+            { AxialDiagonalDirections.Top, new Tuple<int, int>(0, -1) },
+            { AxialDiagonalDirections.TopRight, new Tuple<int, int>(+1, -1) },
+            { AxialDiagonalDirections.BottomRight, new Tuple<int, int>(+1, 0) },
+            { AxialDiagonalDirections.Bottom, new Tuple<int, int>(0, +1) },
+            { AxialDiagonalDirections.BottomLeft, new Tuple<int, int>(-1, +1) },
+            { AxialDiagonalDirections.TopLeft, new Tuple<int, int>(-1, 0) }
+        };
         public Hex()
         {
          
@@ -46,12 +66,24 @@ namespace HexLibrary
             q = column;
         }
 
+        public int DistanceTo(Hex hex)
+        {
+            return (Math.Abs(q + hex.q) + Math.Abs(r - hex.r) + Math.Abs(q + r - hex.q - hex.r)) / 2;
+        }
         public Hex Neighbour(AxialDirections direction)
         {
             var (deltaQ, deltaR) = Directions[direction];
             var neighbour = new Hex {q = q + deltaQ, r = r + deltaR};
             return neighbour;
 
+        }
+
+        
+        public Hex Neighbour(AxialDiagonalDirections direction)
+        {
+            var (deltaQ, deltaR) = DiagonalDirections[direction];
+            var neighbour = new Hex { q = q + deltaQ, r = r + deltaR };
+            return neighbour;
         }
     }
 }

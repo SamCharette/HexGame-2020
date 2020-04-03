486c571b007f42bab1a7d8423879abc72f3e1f05
Sam Charette <spamforsam@worldwidesam.net>
2020-04-03 13:19:06 -0400

    Set engine to be able to load the Monte Carlo player

--- MonteCarloPlayer/MonteCarlo.cs
+++ MonteCarloPlayer/MonteCarlo.cs
@@ -6,11 +6,30 @@ namespace MonteCarloPlayer
     public class MonteCarlo : Player
     {
 
+        public MonteCarlo(int playerNumber, int boardSize, Config playerConfig) 
+            : base(playerNumber, boardSize, playerConfig)
+        {
+            Size = boardSize;
+            PlayerNumber = playerNumber;
+            Setup(playerConfig);
+        }
+
+
         public override Tuple<int, int> SelectHex(Tuple<int, int> opponentMove)
         {
 
 
             return null;
+        }
+
+        private void Setup(Config playerConfig)
+        {
+
+        }
+
+        public override string CodeName()
+        {
+            return "Melece";
         }
     }
 }
